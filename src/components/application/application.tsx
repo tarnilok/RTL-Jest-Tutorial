2a5aca05b5f3941e8e1eae0923e1fc6034626d06
koray <koray.eker@westerops.com>
2022-12-16 08:53:10 +0300

    Rtl queries

--- src/components/application/application.tsx
+++ src/components/application/application.tsx
@@ -14,7 +14,7 @@ export const Application = () => {
               type="text"
               id="name"
               placeholder="Fullname"
-              value="Vishwas"
+              value="Chris"
               onChange={() => {}}
             />
           </div>
@@ -23,7 +23,8 @@ export const Application = () => {
             <textarea id="bio" />
           </div>
           <div>
-            <label htmlFor="job-location">Job location</label>
+            {/* <label htmlFor="job-location">Job location</label> */}
+            <label htmlFor="job-location">Name</label>
             <select id="job-location">
               <option value="">Select a country</option>
               <option value="US">United States</option>
